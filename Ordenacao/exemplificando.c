ee9b2d8cc2d35f59b70f4e1068a74dec869bdf21
Jeferson <souzajefison@gmail.com>
2020-05-13 23:24:43 -0300

    up

--- Ordenacao/exemplificando.c
+++ Ordenacao/exemplificando.c
@@ -6,15 +6,15 @@ int main(void)
     int tamanho = 9;
     int opcao;
     OBJETO *vetorTeste = malloc(sizeof(OBJETO) * tamanho);
-    inserirAleatoriosNoVetor(vetorTeste,tamanho);
+    inserirAleatoriosNoVetor(vetorTeste, tamanho);
     printf("Vetor desordenado:\t");
     printVetor(vetorTeste, tamanho);
     printf("bubble[1]\nselection[2]\ninsertion[3]\n");
-    scanf("%d",&opcao);
+    scanf("%d", &opcao);
     if (opcao == 1)
-        bubble(vetorTeste,tamanho);
-    else if (opcao ==2)   
-        selection(vetorTeste,tamanho);
+        bubble(vetorTeste, tamanho);
+    else if (opcao == 2)
+        selection(vetorTeste, tamanho);
     printf("Vetor ordenado:\t\t");
     printVetor(vetorTeste, tamanho);
     return 0;
